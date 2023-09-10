115641ae9619e3140401e95c7c618a6cc56428e2
Keenan Deyce <keenandeyce@gmail.com>
2023-09-10 17:18:07 +0200

    Update
    - Finished Code Setup
    - Fetched data from GraphQL
    - Currently displaying posts.

--- cms_blog/components/PostCard.jsx
+++ cms_blog/components/PostCard.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment/moment';
+import Link from 'next/link';
 
 const PostCard = ({ post }) => {
   return (
-    <div>
-        {post.title}
-        {post.excerpt}
+    <div className='bg-gradient-to-right-top from-purple-800 to-black shadow-black shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
+      <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
+        <img 
+          src={post.featuredImage.url}
+          alt={post.title}
+          className='object-top absolute h-auto w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg' 
+        />
+      </div>
+      <h1 
+        className='transition duration-700 text-center mb-8 cursor-pointer hover:text-[#5483b3] text-3xl font-semibold'
+      >
+        <Link href={`/post/${post.slug}`}>
+          {post.title}
+        </Link>
+      </h1>
     </div>
   )
 }
--- cms_blog/pages/index.js
+++ cms_blog/pages/index.js
@@ -1,13 +1,13 @@
 import Head from "next/head";
-import { PostCard, PostWidget, Categories } from "../../components";
-
+import { PostCard, PostWidget, Categories, Header } from "../components";
+import { getPosts } from "../services";
 
 const posts = [
   { title: 'React Testing', excerpt: 'Learn React Testing' },
   { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
 ];
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
@@ -15,11 +15,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => <PostCard post={post} key={post.title} />)}
+        <div className="lg:col-span-8 col-span-1 text-white">
+          {posts.map((post) => <PostCard post={post.node} key={post.title} />)}
         </div>
         <div className="lg:col-span-4 col-span-1">
-          <div className="lg:sticky relative top-8">
+          <div className="lg:sticky relative top-8 text-white">
             <PostWidget />
             <Categories />
           </div>
@@ -29,3 +29,11 @@ export default function Home() {
     </div>
   )
 }
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
+} 
