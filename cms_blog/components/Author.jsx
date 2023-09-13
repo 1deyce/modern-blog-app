c6e66ec75f7dd59dab8b29f99a3a590bbd6f27b4
Keenan Deyce <keenandeyce@gmail.com>
2023-09-13 15:46:16 +0200

    Updates
    - Author component
    - CommentsForm component start

--- cms_blog/components/Author.jsx
+++ cms_blog/components/Author.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Image from 'next/image';
 
-const Author = () => {
+const graphCMSImageLoader = ({ src }) => src;
+
+const Author = ({ author }) => {
     return (
-        <div>
-            <h1>Author</h1>
+        <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-30'>
+            <div className='absolute left-0 right-0 -top-14'>
+                <Image
+                    className='align-middle rounded-full'
+                    unoptimized
+                    loader={graphCMSImageLoader}
+                    src={author.photo.url} 
+                    alt={author.name}
+                    height={100}
+                    width={100} 
+                />
+            </div>
+            <h3 className='text-white my-4 text-xl font-bold'>{author.name}</h3>
+            <p className='text-white text-lg'>{author.bio}</p>
         </div>
     )
 }
--- cms_blog/components/PostDetail.jsx
+++ cms_blog/components/PostDetail.jsx
@@ -67,7 +67,7 @@ const PostDetail = ({ post }) => {
                     <div className='font-medium text-gray-500'>
                         <svg 
                             xmlns="http://www.w3.org/2000/svg" 
-                            className="h-6 w-6 inline mr-2 text-[#c1e8ff]" 
+                            className="h-6 w-6 inline mr-2 text-[#021024]" 
                             fill="none" 
                             viewBox="0 0 24 24" 
                             stroke="currentColor"
